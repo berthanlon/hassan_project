ffcb88a06ab225de3587943abfb17f5aab1c0787
archiehanlon12 <archie.hanlon12@gmail.com>
2026-03-28 12:32:30 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+RouteMax — 2-Opt Delivery Route Optimiser
+
+Entry point. Run this file to start the application:
+    python main.py
+
+"""
+
+from app import RouteMaxApp
+
+if __name__ == "__main__":
+    app = RouteMaxApp()
+    app.run()
